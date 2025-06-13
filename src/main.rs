a080441b350ab06e916d9cbe232dace496aab7b7
ahaoboy <504595380@qq.com>
2025-06-13 15:54:24 +0800

    init

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,16 @@ use mozjs::rooted;
 use mozjs::rust::{JSEngine, RealmOptions, Runtime, SIMPLE_GLOBAL_CLASS};
 
 fn main() {
+    let Some(path) = std::env::args().nth(1) else {
+        println!("usage: mozjs <FILE.js>");
+        return;
+    };
+
+    let Ok(javascript) = std::fs::read_to_string(&path) else {
+        println!("failed to read {path}");
+        return;
+    };
+
     let engine = JSEngine::init().unwrap();
     let runtime = Runtime::new(engine.handle());
     let context = runtime.cx();
@@ -40,33 +50,43 @@ fn main() {
         );
         assert!(!function.is_null());
 
-        // let javascript = "print('Test Iñtërnâtiônàlizætiøn ┬─┬ノ( º _ ºノ) ');";
-        let javascript = std::fs::read_to_string("/root/js-engine-benchmark/dist/run.js").unwrap();
         rooted!(in(context) let mut rval = UndefinedValue());
-        assert!(runtime
-            .evaluate_script(global.handle(), &javascript, "test.js", 0, rval.handle_mut())
-            .is_ok());
+        assert!(
+            runtime
+                .evaluate_script(
+                    global.handle(),
+                    &javascript,
+                    "test.js",
+                    0,
+                    rval.handle_mut()
+                )
+                .is_ok()
+        );
     }
 }
 
-unsafe extern "C" fn print(context: *mut JSContext, argc: u32, vp: *mut Value) -> bool { unsafe {
-    let args = CallArgs::from_vp(vp, argc);
+unsafe extern "C" fn print(context: *mut JSContext, argc: u32, vp: *mut Value) -> bool {
+    unsafe {
+        let args = CallArgs::from_vp(vp, argc);
 
-    if args.argc_ != 1 {
-        JS_ReportErrorASCII(context, c"print() requires exactly 1 argument".as_ptr());
-        return false;
+        if args.argc_ != 1 {
+            JS_ReportErrorASCII(context, c"print() requires exactly 1 argument".as_ptr());
+            return false;
+        }
+
+        let arg = mozjs::rust::Handle::from_raw(args.get(0));
+        let js = mozjs::rust::ToString(context, arg);
+        rooted!(in(context) let message_root = js);
+        unsafe extern "C" fn cb(message: *const core::ffi::c_char) {
+            unsafe {
+                let message = CStr::from_ptr(message);
+                let message = str::from_utf8(message.to_bytes()).unwrap();
+                println!("{}", message);
+            }
+        }
+        EncodeStringToUTF8(context, message_root.handle().into(), cb);
+
+        args.rval().set(UndefinedValue());
+        true
     }
-
-    let arg = mozjs::rust::Handle::from_raw(args.get(0));
-    let js = mozjs::rust::ToString(context, arg);
-    rooted!(in(context) let message_root = js);
-    unsafe extern "C" fn cb(message: *const core::ffi::c_char) { unsafe {
-        let message = CStr::from_ptr(message);
-        let message = str::from_utf8(message.to_bytes()).unwrap();
-        println!("{}", message);
-    }}
-    EncodeStringToUTF8(context, message_root.handle().into(), cb);
-
-    args.rval().set(UndefinedValue());
-    true
-}}
+}
